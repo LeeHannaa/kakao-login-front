cc8668c82f9bcf9165f9e3a8d5c357bfd4ba6908
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2024-12-02 00:25:12 +0900

    error: refreshToken 발급 후 저장하지 않았던 오류 해결

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,12 +1,17 @@
 import axios from "axios";
 import decodeToken from "../utils/decodeToken";
+import { useSetRecoilState } from "recoil";
+import { tokenState, refreshTokenState } from "../atom";
 
 const useApi = (refreshToken) => {
+  const setToken = useSetRecoilState(tokenState);
+  const setRefreshToken = useSetRecoilState(refreshTokenState);
+
   const isTokenExpired = (token) => {
     const decoded = decodeToken(token);
     if (!decoded || !decoded.exp) return true; // 토큰 형식이 잘못되었거나 `exp`가 없으면 만료로 간주
-    const currentTime = Math.floor(Date.now() / 1000); // 현재 시간을 초 단위로
-    return decoded.exp < currentTime;
+    const currentTime = Math.floor(Date.now() / 1000);
+    return decoded.exp - 2 < currentTime;
   };
 
   const sendRequest = async (accessToken, endpoint, method) => {
@@ -15,21 +20,37 @@ const useApi = (refreshToken) => {
       "Content-Type": "application/json",
     };
 
-    // 유효기간 확인 및 토큰 추가
     if (isTokenExpired(accessToken)) {
+      // 토큰 만료
       headers["x-refresh-token"] = refreshToken;
-    }
-
-    try {
-      const response = await axios({
-        url: `${process.env.REACT_APP_BASE_URL}${endpoint}`,
-        method,
-        headers,
-      });
-      return response.data;
-    } catch (error) {
-      console.error("API Request failed:", error);
-      throw error;
+      try {
+        const response = await axios({
+          url: `${process.env.REACT_APP_BASE_URL}${endpoint}`,
+          method,
+          headers,
+        });
+        const accessToken = response.headers["authorization"].split(" ")[1]; // Authorization 헤더
+        const refreshToken = response.headers["x-refresh-token"]; // X-Refresh-Token 헤더
+        setToken(accessToken);
+        setRefreshToken(refreshToken);
+        return response.data;
+      } catch (error) {
+        console.error("API Request failed:", error);
+        throw error;
+      }
+    } else {
+      // 토큰 미완료
+      try {
+        const response = await axios({
+          url: `${process.env.REACT_APP_BASE_URL}${endpoint}`,
+          method,
+          headers,
+        });
+        return response.data;
+      } catch (error) {
+        console.error("API Request failed:", error);
+        throw error;
+      }
     }
   };
 
